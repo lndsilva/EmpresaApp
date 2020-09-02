f144fd12594331182e4d610069eff5880c7b2ebc
Laercio-pc <lndsilva@hotmail.com>
2020-09-02 20:20:31 -0300

    login e telas

---
+++ app/src/main/java/br/com/local/empresaapp/Gerenciar_Pessoas_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.empresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gerenciar_Pessoas_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gerenciar__pessoas_layout);
+    }
+}
---
+++ app/src/main/java/br/com/local/empresaapp/Menu_Principal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.empresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu__principal_layout);
+    }
+}
